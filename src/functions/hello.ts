b46bf0ab908280d9643f7cf50e457eb8d5e6c022
Sven Wiegand <source@sven-wiegand.de>
2024-11-17 08:23:42 +0100

    Get IP address of client. Used later on for mixpanel.

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -4,8 +4,9 @@ export async function hello(request: HttpRequest, context: InvocationContext): P
     context.log(`Http function processed request for url "${request.url}"`)
 
     const name = request.query.get('name') || await request.text() || 'world'
+    const ipAddress = request.headers.get('x-forwarded-for') || 'not available';
 
-    return {body: `Hello, ${name}!`}
+    return {body: `Hello, ${name}!\n\nYour IP is ${ipAddress}`}
 }
 
 app.http('Hello', {
